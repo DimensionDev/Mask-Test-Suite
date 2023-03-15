1394e9b991c7103c7595713be2508e8f32822c4a
guanbinrui <guanbinrui@dimension.im>
2023-03-16 00:45:43 +0800

    config

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,21 @@
 import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
+import Logo from './components/Logo'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
-  project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
-  },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
-  footer: {
-    text: 'Nextra Docs Template',
-  },
+    logo: (
+        <>
+            <Logo />
+            <span style={{ marginLeft: 8 }}>Mask Network Test Suite</span>
+        </>
+    ),
+    project: {
+        link: 'https://github.com/DimensionDev/Maskbook',
+    },
+    docsRepositoryBase: 'https://github.com/DimensionDev/Maskbook',
+    footer: {
+        text: 'Nextra Docs Template',
+    },
 }
 
 export default config
